5c24a55d26c1a07e0b60cd1ce1b24b6e6b0e14c0
AK Webs <aeny.dev@gmail.com>
2021-07-06 06:38:17 +0530

    design changed

--- src/components/single-product/categories.js
+++ src/components/single-product/categories.js
@@ -1,5 +1,5 @@
 import { isEmpty, isArray } from 'lodash';
-
+import Link from 'next/link';
 const Categories = ({category}) => {
 
     if ( isEmpty(category) || ! isArray( category ) ) {
@@ -10,9 +10,13 @@ const Categories = ({category}) => {
                 {
                     category.map( (item) => {
                         return (
-                            <span key={item?.id}>
-                                <span>{item?.name}</span>
-                            </span>
+                            <Link href={`/category/${item?.slug}`}>
+                                <a>
+                                    <span key={item?.id}>
+                                        <span>{item?.name}</span>
+                                    </span>                                            
+                                </a>
+                            </Link>
                         )
                     })
                 }
